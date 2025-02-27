317f043b849ef81c9cd4a451450f7ba2568dc9ef
Kamel Faour <10121641@mu.edu.lb>
2025-02-28 00:45:00 +0200

    modified routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
+// User routes (public)
 Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+
+// Auth routes (public)
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Auth routes (protected)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+});
+
